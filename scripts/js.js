9b230c751cf2e4dd4d4a4adcf2c9dfda2578607d
Venlafaxine <506610383@qq.com>
2022-07-26 16:48:55 +0800

    2022.07.26

--- scripts/js.js
+++ scripts/js.js
@@ -1,1 +1,12 @@
 document.documentElement.style.overflow='hidden';
+
+window.onload=function(){
+    if(document.documentElement.scrollHeight <= document.documentElement.clientHeight) {
+      var bodyTag = document.getElementsByTagName('sss')[0];
+    //   bodyTag.style.height = document.documentElement.clientWidth / screen.width * screen.height + 'px';
+      bodyTag.style.height =  screen.height * 1.03 + 'px';
+    }
+    setTimeout(function() {
+      window.scrollTo(0, 1)
+    }, 0);
+  };
